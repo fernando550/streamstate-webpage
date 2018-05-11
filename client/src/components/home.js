7e33bd9fb72fb77cf6ac6bfc22f022c167eb056c
Fernando Narbona <fnarbona@tracfone.com>
2018-05-11 15:15:34 -0400

    updated homepage text

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -9,14 +9,16 @@ class Home extends Component {
                 <p style={{fontSize: '25px'}}>Developing practical solutions to impractical problems</p>
             </div>
             <div style={{width: '50%'}}>
-                <p style={{fontSize: '18px'}}>We develop with your needs in mind.</p>
-                <p style={{fontSize: '18px'}}>Engineers and analysts on our team possess a passion for learning
-                  and the pursuit of excellence. Fine tuning your extensive needs,
-                  building trusting relationships, and executing effectively are
-                  the three drivers for our consulting style.</p>
+                <p style={{fontSize: '18px'}}>
+                    Engineers and analysts on our team possess a passion for learning
+                    and the pursuit of excellence. Fine tuning your extensive needs,
+                    building trusting relationships, and executing effectively are
+                    the three drivers for our consulting style.</p>
                 <br/>
-                <p style={{fontSize: '18px'}}>The end result? A solution that has been delivered with you in mind, but nothing short of
-                    performant.</p>
+                <p style={{fontSize: '25px'}}>
+                    The end result?</p>
+                <p style={{fontSize: '18px'}}>
+                    A solution that has been delivered with you in mind, but nothing short of performant.</p>
             </div>
         </div>
       </div>
