176d776af09668ac4a62cd63738ff5f0910a9310
Fernando Rafael Narbona <fernandorafaelnarbona@MacBook-Pro.local>
2018-06-16 14:19:06 -0400

    initial launch navbar issues

--- client/src/layout/footer.js
+++ client/src/layout/footer.js
@@ -7,33 +7,34 @@ class Header extends Component {
     // const fbLink = "https://www.facebook.com/azimutinsurance/";
     // const instaLink = "https://instagram.com/azimutinsurance";
     // const targetTab = "_blank";
+    const email = "STREAMSTATECONSULTING@GMAIL.COM"
 
     return (
       <div id="footer component">
         <div class="filler-div" style={{height: '300px'}}></div>
         <div id="footer-content" class="indigo darken-3 row">
 
-          <div class="footer-box footer-border col s4">
+          <div class="footer-box col m4">
               <h4>LOCATION <i class="material-icons">location_on</i></h4>
               <div>MAIN OFFICE</div>
-              <div>10585 NW 43RD TER, MIAMI, FL 33178</div>
+              <div>MIAMI, FL</div>
           </div>
 
-          <div class="footer-box footer-border col s4">
+          <div class="footer-box col m4">
               <h4>CONTACT <i class="material-icons"> phonelink</i></h4>
               <div>EMAIL</div>
-              <div>STREAMSTATECONSULTING@GMAIL.COM</div>
+              <div>{email}</div>
               <br/>
               <div>PHONE</div>
               <div>(786) 344 - 7889</div>
           </div>
-          <div class="footer-box footer-border col s4">
+          <div class="footer-box col m4">
               <h4>SOCIAL MEDIA <i class="material-icons"> share</i></h4>
               <div>FACEBOOK</div>
-              <div><a herf="#">FB LINK</a></div>
+              <div><a herf="#">FB LINK (under construction)</a></div>
               <br/>
               <div>INSTAGRAM</div>
-              <div><a herf="#">INSTAGRAM</a></div>
+              <div><a herf="#">INSTAGRAM (under construction)</a></div>
           </div>
         </div>
       </div>
